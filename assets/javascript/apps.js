2971e4bcc0e07ca9d3ee69cd3ca1bf05024b0968
Sabrina Rocha <delrock876@gmail.com>
2019-08-29 16:12:26 -0700

    added workbank

---
+++ assets/javascript/apps.js
@@ -0,0 +1,7 @@
+const wordBank = [ 'familiar', 'salem', 'hysteria', 'ritual', 'incantation', 'tarot', 'supernatural', 'nature', 'cauldron', 'suspicion', 'noose', 'sabbath', 'magic', 'warlock', 'rumor', 'knowledge', 'spell', 'evil', 'demon', 'witch', 'broom', 'potion' ]
+
+
+
+wordBank.forEach(function(elem){
+  console.log('hi')
+})
