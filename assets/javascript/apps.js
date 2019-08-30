09bd3520f42c2ceb1fa03997f13b8edc8d885c7b
Sabrina Rocha <delrock876@gmail.com>
2019-08-30 01:29:04 -0700

    added underscore & letter placement functionality

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,7 +1,52 @@
 const wordBank = [ 'familiar', 'salem', 'hysteria', 'ritual', 'incantation', 'tarot', 'supernatural', 'nature', 'cauldron', 'suspicion', 'noose', 'sabbath', 'magic', 'warlock', 'rumor', 'knowledge', 'spell', 'evil', 'demon', 'witch', 'broom', 'potion' ]
 
+//pick random word from array
+let grabbedWord = wordBank[Math.floor(Math.random()* wordBank.length)]
+let wins = 0
+let guessRemain = 10
+let letterGuessed = []
+let underscores = []
+let remainingLetters = grabbedWord.length;
+
+console.log(grabbedWord)//comment out later
 
 
-wordBank.forEach(function(elem){
-  console.log('hi')
-})
+//generate underscore for # of letters in words
+for (let i = 0; i < grabbedWord.length; i++){
+  underscores.push("_")
+}
+console.log(underscores) //comment out later
+
+document.getElementById('wins').innerHTML = wins
+document.getElementById('under').innerHTML = underscores.join(' ')
+document.getElementById('guessesLeft').innerHTML = 10
+document.getElementById('lettersGuessed').innerHTML = ''
+
+
+//        **** MAIN GAME ****       //
+
+
+ document.onkeypress = function runGame(event){
+  let guessedLetter = event.key
+ 
+  if (97 <= event.keyCode && event.keyCode <= 122){
+
+    // if(remainingLetters > 0){
+
+    //   document.getElementById('under').textContent= underscores
+    // }else {
+    //   //end the game
+    // }
+//grabs and replaces underscore with correctly guessed letter
+ for(let j = 0 ; j < grabbedWord.length; j++){
+    if(grabbedWord[j] === guessedLetter){
+      underscores[j] = guessedLetter
+      document.getElementById('under').innerHTML = underscores.join(' ')
+      guessRemain--
+      remainingLetters--
+    }
+ }
+
+
+}  
+ }
