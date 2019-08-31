dfb5e93f53110442cce19942bc854a4729e62959
Sabrina Rocha <delrock876@gmail.com>
2019-08-30 19:42:55 -0700

    comment  out else condition

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,7 +1,7 @@
-const wordBank = [ 'familiar', 'salem', 'hysteria', 'ritual', 'incantation', 'tarot', 'supernatural', 'nature', 'cauldron', 'suspicion', 'noose', 'sabbath', 'magic', 'warlock', 'rumor', 'knowledge', 'spell', 'evil', 'demon', 'witch', 'broom', 'potion' ]
+const wordBank = ['familiar', 'salem', 'hysteria', 'ritual', 'incantation', 'tarot', 'supernatural', 'nature', 'cauldron', 'suspicion', 'noose', 'sabbath', 'magic', 'warlock', 'rumor', 'knowledge', 'spell', 'evil', 'demon', 'witch', 'broom', 'potion']
 
 //pick random word from array
-let grabbedWord = wordBank[Math.floor(Math.random()* wordBank.length)]
+let grabbedWord = wordBank[Math.floor(Math.random() * wordBank.length)]
 let wins = 0
 let guessRemain = 10
 let letterGuessed = []
@@ -12,7 +12,7 @@ console.log(grabbedWord)//comment out later
 
 
 //generate underscore for # of letters in words
-for (let i = 0; i < grabbedWord.length; i++){
+for (let i = 0; i < grabbedWord.length; i++) {
   underscores.push("_")
 }
 console.log(underscores) //comment out later
@@ -26,27 +26,33 @@ document.getElementById('lettersGuessed').innerHTML = ''
 //        **** MAIN GAME ****       //
 
 
- document.onkeypress = function runGame(event){
+document.onkeypress = function runGame(event) {
   let guessedLetter = event.key
- 
-  if (97 <= event.keyCode && event.keyCode <= 122){
+
+  if (97 <= event.keyCode && event.keyCode <= 122) {
 
     // if(remainingLetters > 0){
 
-    //   document.getElementById('under').textContent= underscores
+    //   
     // }else {
     //   //end the game
     // }
-//grabs and replaces underscore with correctly guessed letter
- for(let j = 0 ; j < grabbedWord.length; j++){
-    if(grabbedWord[j] === guessedLetter){
-      underscores[j] = guessedLetter
-      document.getElementById('under').innerHTML = underscores.join(' ')
-      guessRemain--
-      remainingLetters--
+
+    //grabs and replaces underscore with correctly guessed letter
+    for (let j = 0; j < grabbedWord.length; j++) {
+      if (grabbedWord[j] === guessedLetter) {
+        underscores[j] = guessedLetter
+        document.getElementById('under').innerHTML = underscores.join(' ')
+        guessRemain--
+        document.getElementById('guessesLeft').innerHTML = guessRemain
+        remainingLetters--
+      }
+      // else {
+      //   letterGuessed.push(guessedLetter)
+      //   document.getElementById('lettersGuessed').innerHTML = letterGuessed.join(' ')
+      // }
+
     }
- }
 
-
-}  
- }
+  }
+}
